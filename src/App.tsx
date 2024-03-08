d57970d7b17025b398df19dccefe575051d085ec
Onur Ucar <hello@ucaronur.com>
2024-03-08 12:40:43 -0500

    Highlighting errors, resetting iframe contents and passing code from parent to the iframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { useState, useEffect, useRef } from 'react';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import * as esbuild from 'esbuild-wasm';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import { useState, useEffect, useRef } from "react";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import * as esbuild from "esbuild-wasm";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 
 function App() {
   const ref = useRef<any>();
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
+  const iframe = useRef<any>();
+  const [input, setInput] = useState("");
+  // const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
   useEffect(() => {
@@ -23,29 +24,43 @@ function App() {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
+
     // starting the bundling process
     const result = await ref.current.build({
-      entryPoints: ['index.js'],
+      entryPoints: ["index.js"],
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       // whenever you see process.env.NODE_ENV, replace it with "production"
       define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
       },
     });
-
-
-    setCode(result.outputFiles[0].text);
-
-
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
 
   const html = `
-  <script>
-    ${code}
-  </script>
+  <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event) => {
+          try {
+            eval(event.data);
+          } catch (err) {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err);
+          }
+        }, false);
+      </script>
+    </body>
+  </html>
 `;
 
   return (
@@ -57,11 +72,14 @@ function App() {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      {/* Remove later */}
-      <iframe sandbox='allow-scripts' srcDoc={html} />
+      <iframe
+        title="code preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 }
 
-export default App
+export default App;
