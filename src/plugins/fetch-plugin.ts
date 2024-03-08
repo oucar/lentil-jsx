cf62495e83c76dbfcc479c69dc5e3fc2ec295aea
Onur Ucar <hello@ucaronur.com>
2024-03-07 23:28:16 -0500

    Added seperate onLoad filters and extracted common caching logic to a different onLoad method

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,14 +10,17 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
-        }
+      // will eventually call multiple onLoad functions until one of them returns a result
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
 
+      // for returning an already cached result
+      // @@TODO: Add a feature so that users can clear their caches with a click of a button
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
@@ -25,23 +28,38 @@ export const fetchPlugin = (inputCode: string) => {
         if (cachedResult) {
           return cachedResult;
         }
+      });
+
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
-        // loading css files if they are requested
-        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
-        // handling css files by tricking esbuild into thinking it's a jsx file and then adding the style tag to the head
-        const contents =
-          fileType === "css"
-            ? `
-        const style = document.createElement('style');
-        style.innerText = 'body { background-color: "red" }';
-        document.head.appendChild(style);
-        `
-            : data;
+        const escapedCss = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${escapedCss}';
+          document.head.appendChild(style);
+        `;
 
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
         await fileCache.setItem(args.path, result);
