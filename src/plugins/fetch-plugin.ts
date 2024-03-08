73d0b4e22b43b9c9c907c38763afe75b288b7b62
Onur Ucar <hello@ucaronur.com>
2024-03-07 23:05:31 -0500

    CSS Handling implementation

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -1,19 +1,19 @@
-import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
-import localForage from 'localforage';
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
+import localForage from "localforage";
 
 const fileCache = localForage.createInstance({
-  name: 'filecache',
+  name: "filecache",
 });
 
 export const fetchPlugin = (inputCode: string) => {
   return {
-    name: 'fetch-plugin',
+    name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
+        if (args.path === "index.js") {
           return {
-            loader: 'jsx',
+            loader: "jsx",
             contents: inputCode,
           };
         }
@@ -27,10 +27,22 @@ export const fetchPlugin = (inputCode: string) => {
         }
         const { data, request } = await axios.get(args.path);
 
+        // loading css files if they are requested
+        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
+        // handling css files by tricking esbuild into thinking it's a jsx file and then adding the style tag to the head
+        const contents =
+          fileType === "css"
+            ? `
+        const style = document.createElement('style');
+        style.innerText = 'body { background-color: "red" }';
+        document.head.appendChild(style);
+        `
+            : data;
+
         const result: esbuild.OnLoadResult = {
-          loader: 'jsx',
-          contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname,
+          loader: "jsx",
+          contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
         };
         await fileCache.setItem(args.path, result);
 
