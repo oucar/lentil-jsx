eca52e5d294e78d8be132119e2f5e874840780b1
Onur Ucar <hello@ucaronur.com>
2024-03-11 20:54:06 -0400

    Added progress bar to code cell when bundling bigger packages and styling

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -5,6 +5,7 @@ import Resizable from "./Resizable";
 import { Cell } from "../state";
 import { useActions } from "../hooks/use-actions";
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import "./styles/codeCell.css";
 
 interface CodeCellProps {
   cell: Cell;
@@ -17,6 +18,13 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   // Bundle the code every 1000 ms, only if the input has changed
   // @@TODO: Add a loading spinner, and possibly allow a greater delay before bundling
   useEffect(() => {
+    // when we first load the app, we want to bundle the code immediately
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+
+    // when user starts the coce cell
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 1000);
@@ -26,7 +34,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return () => {
       clearTimeout(timer);
     };
-  }, [cell.content, cell.id, createBundle]);
+  }, [cell.content, cell.id]);
 
   return (
     <Resizable direction="vertical">
@@ -37,7 +45,18 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} bundlingError={bundle.err} />}
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            // Loading spinner
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} bundlingError={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
